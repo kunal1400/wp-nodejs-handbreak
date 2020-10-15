a5f29334cc1d6dfbf206b64677057512f06ded74
kunal1400 <kunal.malviya351@gmail.com>
2020-10-15 17:42:16 +0530

    fixed the mp3 and mp4 bugs

--- handbrake.js
+++ handbrake.js
@@ -7,8 +7,8 @@ const convertFileExtenstion = ( file, extenstion="mp4" ) => {
 	return file.substr(0, pos < 0 ? file.length : pos) + "." + extenstion;
 }
 
-const processFile = ( uploadedDirectory, uploadedFileName, cb ) => {
-	let renamedFile = convertFileExtenstion(uploadedFileName)
+const processVideoFile = ( uploadedDirectory, uploadedFileName, mimeType, cb ) => {
+	let renamedFile = convertFileExtenstion(uploadedFileName, "mp4")
 	if ( uploadedDirectory ) {
 		let spawnData = {
 			input: `${uploadedDirectory}/${uploadedFileName}`,
@@ -28,25 +28,81 @@ const processFile = ( uploadedDirectory, uploadedFileName, cb ) => {
 			//	}
 			//});
 			hbjs.spawn(spawnData)
-                        .on("begin",function(){
-                                console.log('begin')
-                        })
-                        .on('error', err => {
-                                // invalid user input, no video found etc
-                                console.log(err, "err")
-                                reject(err)
-                        })
-                        .on('progress', progress => {
-                                console.log('Percent complete: %s, ETA: %s', progress.percentComplete, progress.eta)
-                        })
-                        .on("complete", function (complete) {
-                                console.log('complete');
-                                resolve({modified_file: renamedFile, 
+            .on("begin",function(){
+                console.log('begin')
+            })
+            .on('error', err => {
+                // invalid user input, no video found etc
+                console.log(err, "err")
+                reject(err)
+            })
+            .on('progress', progress => {
+                console.log('Percent complete: %s, ETA: %s', progress.percentComplete, progress.eta)
+            })
+            .on("complete", function (complete) {
+                console.log('complete');
+                resolve({modified_file: renamedFile, 
 					original_file: uploadedFileName, 
 					modified_filepath:`${uploadedDirectory}/${renamedFile}`, 
 					original_filepath:`${uploadedDirectory}/${uploadedFileName}`
 				})
-                        })
+            })
+		})
+		.then(function(data) {
+			console.log("2. Data is ready for callback Success!!", data)
+			fs.unlinkSync(data.original_filepath)
+			cb(null, data)
+		})
+		.catch(function(errorData) {
+			console.log("3. ", errorData);
+			cb(errorData, null)
+		})
+	}
+	else {
+		console.log("2. File not found")
+	}
+}
+
+const processAudioFile = ( uploadedDirectory, uploadedFileName, mimeType, cb ) => {
+	let renamedFile = convertFileExtenstion(uploadedFileName, "mp3")
+	if ( uploadedDirectory ) {
+		let spawnData = {
+			input: `${uploadedDirectory}/${uploadedFileName}`,
+			output: `${uploadedDirectory}/${renamedFile}`
+		}
+
+		console.log("1.", spawnData, "spawnData")
+
+		new Promise(function(resolve, reject) {
+			//STEP 3: Rename the uploaded file and put old suffix
+			//fs.rename(spawnData.input, spawnData.output, (err) => {
+			//	if (!err) {
+			//		resolve('Rename complete!')
+			//	}
+			//	else {
+			//		reject(err);
+			//	}
+			//});
+			hbjs.spawn(spawnData)
+            .on("begin",function(){
+                console.log('begin')
+            })
+            .on('error', err => {
+                // invalid user input, no video found etc
+                console.log(err, "err")
+                reject(err)
+            })
+            .on('progress', progress => {
+                console.log('Percent complete: %s, ETA: %s', progress.percentComplete, progress.eta)
+            })
+            .on("complete", function (complete) {
+                console.log('complete');
+                resolve({modified_file: renamedFile, 
+					original_file: uploadedFileName, 
+					modified_filepath:`${uploadedDirectory}/${renamedFile}`, 
+					original_filepath:`${uploadedDirectory}/${uploadedFileName}`
+				})
+            })
 		})
 		.then(function(data) {
 			console.log("2. Data is ready for callback Success!!", data)
@@ -64,5 +120,6 @@ const processFile = ( uploadedDirectory, uploadedFileName, cb ) => {
 }
 
 module.exports = {
-	processFile
+	processVideoFile,
+	processAudioFile
 }
--- index.js
+++ index.js
@@ -2,19 +2,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 const hb = require('./handbrake')
+const videoMimeTypes = ['video','video/x-flv', 'video/mp4', 'application/x-mpegURL', 'video/MP2T', 'video/3gpp', 'video/quicktime', 'video/x-msvideo', 'video/x-ms-wmv', 'video/avi']
+const audioMimeTypes = ['audio', 'audio/basic','auido/L24','audio/mid','audio/mpeg','audio/mp4','audio/x-aiff','audio/x-mpegurl','audio/vnd.rn-realaudio','audio/vnd.rn-realaudio','audio/ogg','audio/vorbis','audio/vnd.wav']
 
 app.get('/', (req, res) => {
 
 	// STEP 1: Get the uploaded file root path (this we get from request data)
 	let uploadedFilePath = req.query.fullsizepath
-	if (!uploadedFilePath) {
+	let mimetype = req.query.mimetype
+
+	if ( !uploadedFilePath ) {
 		res.send({status: false, msg: "uploadedFilePath param is required"})
+	} 
+	else if( !mimetype ) {
+		res.send({status: false, msg: "mimetype param is required"})
 	}
 	else {
 		let uploadedFileName  = ""
 		let uploadedDirectory = []
 
 		if (uploadedFilePath) {
+
 			let splitedArray = uploadedFilePath.split("/")
 			if (splitedArray.length > 0) {
 				uploadedFileName  = splitedArray[splitedArray.length - 1]
@@ -22,14 +30,36 @@ app.get('/', (req, res) => {
 					uploadedDirectory.push(splitedArray[i])
 				}
 			}
-			hb.processFile( uploadedDirectory.join("/"), uploadedFileName, function(error, data) {
-				if (!error) {
-					res.send({status:true, data})
-				}
-				else {
-					res.send({status:false, error})
-				}
-			})
+
+			// If uploaded file is video
+			if ( videoMimeTypes.indexOf(mimetype) !== -1 ) {
+				console.log(mimetype, "videoMimeTypes")
+				
+				hb.processVideoFile( uploadedDirectory.join("/"), uploadedFileName, mimetype, function(error, data) {
+					if (!error) {
+						res.send({status:true, data})
+					}
+					else {
+						res.send({status:false, error})
+					}
+				})
+			}
+			else if ( audioMimeTypes.indexOf(mimetype) !== -1 ) {
+				console.log(mimetype, "audioMimeTypes")
+				
+				// If uploaded file is video
+				hb.processAudioFile( uploadedDirectory.join("/"), uploadedFileName, mimetype, function(error, data) {
+					if (!error) {
+						res.send({status:true, data})
+					}
+					else {
+						res.send({status:false, error})
+					}
+				})
+			}
+			else {
+				res.send({status:false, msg: "Invalid mime type"})
+			}
 		}
 	}
 })
